30cf93a291fa38826185d065793889df5ad80453
DelHubzSakez <milone.reales@gmail.com>
2025-08-02 23:29:56 +0300

    Player Bouncing

--- player.py
+++ player.py
@@ -1,12 +1,39 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS 
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x : int, y : int) -> object:
         self.rotation : int = 0
         super().__init__(x , y, PLAYER_RADIUS)
-         
+    
+    def rotate(self : object, dt : int)->None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+            
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)    
+
+    def move(self : object, dt : int)->None:
+        self.position += (pygame.Vector2(0, 1).rotate(self.rotation) *
+                         PLAYER_SPEED * dt)
+        
+        
+
+                
+        
+
        
     def triangle(self): 
         forward : function   = pygame.Vector2(0, 1).rotate(self.rotation)
