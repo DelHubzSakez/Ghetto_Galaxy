85c930993a0e77d41b00c4abd61e42e7c7f63f7a
DelHubzSakez <milone.reales@gmail.com>
2025-08-03 00:03:58 +0300

    Player Moving + Group Management

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     player : Player = Player(x = SCREEN_WIDTH / 2,
                              y = SCREEN_HEIGHT / 2)
     
+    updateable : object = pygame.sprite.Group(player)
+    drawable   : object = pygame.sprite.Group(player)
+    Player.containers   = (updateable, drawable)
+    
     print("Starting Asteroids!")
     
     while True:
@@ -20,10 +24,11 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for D in drawable:
+            D.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updateable.update(dt)
         
         
      
